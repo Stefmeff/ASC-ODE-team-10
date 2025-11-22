5d5c01a6a93bdd51719278d3ea1c75692bf6ad43
e12025955 <e12025955@student.tuwien.ac.at>
2025-11-22 15:17:17 +0100

    Crank Nicolson

--- src/timestepper.hpp
+++ src/timestepper.hpp
@@ -63,19 +63,48 @@ namespace ASC_ode
   class ImprovedEuler : public TimeStepper
   {
     Vector<> m_vecf;
-    Vector<> y_tmp;
-
   public:
     ImprovedEuler(std::shared_ptr<NonlinearFunction> rhs) 
     : TimeStepper(rhs), m_vecf(rhs->dimF()) {}
     void DoStep(double tau, VectorView<double> y) override
     {
       this->m_rhs->evaluate(y, m_vecf);
-      y_tmp= y + (tau/2) * m_vecf;
+      Vector<> y_tmp = y + (tau/2) * m_vecf;
       this->m_rhs->evaluate(y_tmp, m_vecf);
       y += (tau) * m_vecf;
     }
   };
+
+  /**
+   * @brief Ex. 17.4.1 Crank-Nicolson time stepping method implementation
+   */
+  class CrankNicolson : public TimeStepper
+  {
+    std::shared_ptr<NonlinearFunction> m_equ;
+    std::shared_ptr<Parameter> m_tau;
+    std::shared_ptr<ConstantFunction> m_yold;
+    std::shared_ptr<ConstantFunction> m_vecf;
+    Vector<> f_yold;
+  public:
+    CrankNicolson(std::shared_ptr<NonlinearFunction> rhs) 
+    : TimeStepper(rhs), m_tau(std::make_shared<Parameter>(0.0)),
+      f_yold(rhs->dimF()), m_vecf(std::make_shared<ConstantFunction>(rhs->dimF()))
+    {
+      m_yold = std::make_shared<ConstantFunction>(rhs->dimX());
+      auto ynew = std::make_shared<IdentityFunction>(rhs->dimX());
+      m_equ = ynew - m_yold - m_tau * (m_vecf + m_rhs);
+    }
+
+    void DoStep(double tau, VectorView<double> y) override
+    {
+      m_yold->set(y);
+      m_tau->set(tau*0.5);
+      this->m_rhs->evaluate(y, f_yold);
+      m_vecf->set(f_yold);
+      NewtonSolver(m_equ, y);
+    }
+  };
+
 
 
   
