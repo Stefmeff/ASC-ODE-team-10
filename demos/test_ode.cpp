42d25060a9a476a777bb23938ed24485eaa441aa
Adam Moritz Rasched <e1644619@student.tuwien.ac.at>
2025-12-01 12:32:28 +0100

    ElectricNetwork

--- demos/test_ode.cpp
+++ demos/test_ode.cpp
@@ -37,31 +37,60 @@ public:
 };
 
 //TODO: Exercise 17.4.1
-class  ElectricNetwork : public NonlinearFunction
+class ElectricNetwork : public NonlinearFunction
 {
 private:
-  //TODO Ersezten
-  double Resistance;
-  double Capacitance;
+    // Renamed variables
+    double resistance;
+    double capacitance;
+    double angularFrequency; 
 
 public:
-  MassSpring(double m, double k) : mass(m), stiffness(k) {}
+    // Constructor
+    ElectricNetwork(double R, double C) 
+        : resistance(R), capacitance(C) 
+    {
+        // 100 * PI is the angular frequency (omega)
+        angularFrequency = 100.0 * M_PI;
+    }
 
-  size_t dimX() const override { return 2; }
-  size_t dimF() const override { return 2; }
-  
-  void evaluate (VectorView<double> x, VectorView<double> f) const override
-  {
-    f(0) = x(1);
-    f(1) = -stiffness/mass*x(0);
-  }
-  
-  void evaluateDeriv (VectorView<double> x, MatrixView<double> df) const override
-  {
-    df = 0.0;
-    df(0,1) = 1;
-    df(1,0) = -stiffness/mass;
-  }
+    size_t dimX() const override { return 2; }
+    size_t dimF() const override { return 2; }
+    
+    // f(x)
+    void evaluate (VectorView<double> x, VectorView<double> f) const override
+    {
+        double Uc = x(0); // Voltage
+        double t  = x(1); // Time
+        
+        // dUc/dt = (1/RC) * (U0(t) - Uc)
+        // Using the new variable names here:
+        f(0) = (1.0 / (resistance * capacitance)) * (std::cos(angularFrequency * t) - Uc);
+        
+        // dt/dt = 1
+        f(1) = 1.0; 
+    }
+    
+    // Jacobian J = df/dx
+    void evaluateDeriv (VectorView<double> x, MatrixView<double> df) const override
+    {
+        double t = x(1);
+        
+        // Use new names for calculation
+        double rc_inv = 1.0 / (resistance * capacitance);
+
+        df = 0.0;
+
+        // d(f0)/d(x0) -> d(U'c)/dUc = -1/RC
+        df(0,0) = -rc_inv;
+        
+        // d(f0)/d(x1) -> d(U'c)/dt  = (1/RC) * (-omega * sin(omega*t))
+        df(0,1) = rc_inv * (-angularFrequency * std::sin(angularFrequency * t));
+
+        // d(f1)/... -> derivatives of 1 are 0
+        df(1,0) = 0.0;
+        df(1,1) = 0.0;
+    }
 };
 
 int main(int argc, char** argv)
