94a259b1c565c6fe9879834ef464f64b6454adc2
e12025955 <e12025955@student.tuwien.ac.at>
2025-12-01 12:54:12 +0100

    RungeKutta

--- demos/plotmassspring.py
+++ demos/plotmassspring.py
@@ -10,11 +10,15 @@ for steps in [50, 100, 150, 200]:
     filename2 = f'../outputs/ImprovedEuler/steps{steps}.txt'
     filename3 = f'../outputs/ImplicitEuler/steps{steps}.txt'
     filename4 = f'../outputs/CrankNicolson/steps{steps}.txt'
+    filename5 = f'../outputs/RungeKutta/steps{steps}.txt'
+    filename6 = f'../outputs/ImplicitRungeKutta/steps{steps}.txt'
     d1 = np.loadtxt(filename1, usecols=(0, 1, 2))
     d2 = np.loadtxt(filename2, usecols=(0, 1, 2))
     d3 = np.loadtxt(filename3, usecols=(0, 1, 2))
     d4 = np.loadtxt(filename4, usecols=(0, 1, 2))
-    data = [d1, d2, d3, d4]
+    d5 = np.loadtxt(filename5, usecols=(0, 1, 2))
+    d6 = np.loadtxt(filename6, usecols=(0, 1, 2))
+    data = [d1, d2, d3, d4, d5, d6]
 
 
     import matplotlib.pyplot as plt
@@ -32,6 +36,19 @@ for steps in [50, 100, 150, 200]:
     plt.savefig(f"../outputs/plots/massspring_position_steps{steps}.png")
     plt.clf()
 
+    # Plot Runge Kutta methods
+    plt.plot(data[4][:,0], data[4][:,1], label='position-Explicit Runge Kutta')
+    plt.plot(data[5][:,0], data[5][:,1], label='position-Implicit Runge Kutta')
+    plt.xlabel('time')
+    plt.ylabel('value')
+    plt.title('Mass-Spring System Time Evolution')
+    plt.legend()
+    plt.grid()
+    plt.savefig(f"../outputs/plots/Runge_massspring_position_steps{steps}.png")
+    plt.clf()
+
+
+
     plt.plot(data[0][:,1], data[0][:,2], label='phase plot-explicit Euler')
     plt.plot(data[1][:,1], data[1][:,2], label='phase plot-improved Euler')
     plt.plot(data[2][:,1], data[2][:,2], label='phase plot-implicit Euler')
@@ -44,3 +61,13 @@ for steps in [50, 100, 150, 200]:
     plt.savefig(f"../outputs/plots/massspring_phase_steps{steps}.png")
     plt.clf()
 
+    # Plot Runge Kutta methods phase plot
+    plt.plot(data[4][:,1], data[4][:,2], label='phase plot-Explicit Runge Kutta')
+    plt.plot(data[5][:,1], data[5][:,2], label='phase plot-Implicit Runge Kutta')
+    plt.xlabel('position')
+    plt.ylabel('velocity') 
+    plt.title('Mass-Spring System Phase Plot')
+    plt.legend()
+    plt.grid()
+    plt.savefig(f"../outputs/plots/Runge_massspring_phase_steps{steps}.png")
+    plt.clf()
--- demos/test_ode.cpp
+++ demos/test_ode.cpp
@@ -36,33 +36,6 @@ public:
   }
 };
 
-//TODO: Exercise 17.4.1
-class  ElectricNetwork : public NonlinearFunction
-{
-private:
-  //TODO Ersezten
-  double Resistance;
-  double Capacitance;
-
-public:
-  MassSpring(double m, double k) : mass(m), stiffness(k) {}
-
-  size_t dimX() const override { return 2; }
-  size_t dimF() const override { return 2; }
-  
-  void evaluate (VectorView<double> x, VectorView<double> f) const override
-  {
-    f(0) = x(1);
-    f(1) = -stiffness/mass*x(0);
-  }
-  
-  void evaluateDeriv (VectorView<double> x, MatrixView<double> df) const override
-  {
-    df = 0.0;
-    df(0,1) = 1;
-    df(1,0) = -stiffness/mass;
-  }
-};
 
 int main(int argc, char** argv)
 {
@@ -75,73 +48,81 @@ int main(int argc, char** argv)
       method = "ExplicitEuler"; // default method
   }
 
-    // create the ODE right-hand side
-    auto rhs = std::make_shared<MassSpring>(1.0, 1.0);
+  // create the ODE right-hand side
+  auto rhs = std::make_shared<MassSpring>(1.0, 1.0);
 
-    std::unique_ptr<TimeStepper> stepper;
-    std::string path;
+  std::unique_ptr<TimeStepper> stepper;
+  std::string path;
 
-    if (method=="ImprovedEuler") 
-    {
-      std::cout << "Using Improved Euler Method" << std::endl;
-      stepper = std::make_unique<ImprovedEuler>(rhs);
-      path = "../outputs/ImprovedEuler/";
-    } else if (method=="CrankNicolson") {
-      std::cout << "Using Crank-Nicolson Method" << std::endl;
-      stepper = std::make_unique<CrankNicolson>(rhs);
-      path = "../outputs/CrankNicolson/";
-    } else if (method=="ImplicitEuler") {     
-      std::cout << "Using Implicit Euler Method" << std::endl;
-      stepper = std::make_unique<ImplicitEuler>(rhs);
-      path = "../outputs/ImplicitEuler/";
-    } else {
-      std::cout << "Using Explicit Euler Method" << std::endl;
-      stepper = std::make_unique<ExplicitEuler>(rhs);
-      path = "../outputs/ExplicitEuler/";
-    }
+  Vector<> Radau(3), RadauWeight(3);
+  GaussRadau(Radau, RadauWeight);
+  // not sure about weights, comput them via ComputeABfromC
+  std::cout << "Radau = " << Radau << ", weight = " << RadauWeight <<  std::endl;
+
+  // Initialize Butcher tableaux for Gauss methods
+  Vector<> Gauss2c(2), Gauss3c(3);
+
+  // arbitrary order Gauss-LegendreR
+  int stages = 5;
+  Vector<> c(stages), b1(stages);
+  GaussLegendre(c, b1);
+  auto [a, b] = ComputeABfromC(c);
+
+
+  if (method=="ImprovedEuler") 
+  {
+    std::cout << "Using Improved Euler Method" << std::endl;
+    stepper = std::make_unique<ImprovedEuler>(rhs);
+    path = "../outputs/ImprovedEuler/";
+  } else if (method=="CrankNicolson") {
+    std::cout << "Using Crank-Nicolson Method" << std::endl;
+    stepper = std::make_unique<CrankNicolson>(rhs);
+    path = "../outputs/CrankNicolson/";
+  } else if (method=="ImplicitEuler") {     
+    std::cout << "Using Implicit Euler Method" << std::endl;
+    stepper = std::make_unique<ImplicitEuler>(rhs);
+    path = "../outputs/ImplicitEuler/";
+  } else if(method == "RungeKutta") {
+    std::cout << "Using Runge Kutta Method: " << path << std::endl;
+    stepper = std::make_unique<RungeKutta>(rhs, Gauss2a, Gauss2b, Gauss2c);
+    path = "../outputs/RungeKutta/";
+  } else if(method=="ImplicitRungeKutta") {
+    std::cout << "Using Implicit Runge Kutta: " << path << std::endl;
+    auto [Gauss3a,Gauss3b] = ComputeABfromC (Gauss3c);
+    stepper = std::make_unique<ImplicitRungeKutta>(rhs, Gauss3a, Gauss3b, Gauss3c);
+    path = "../outputs/ImplicitRungeKutta/";
+  } else {
+    std::cout << "Using Explicit Euler Method" << std::endl;
+    stepper = std::make_unique<ExplicitEuler>(rhs);
+    path = "../outputs/ExplicitEuler/";
+  } 
 
   double tend = 4*M_PI;
   int steps[] = {50, 100, 150, 200};
 
-
-      Vector<> Radau(3), RadauWeight(3);
-      GaussRadau (Radau, RadauWeight);
-      // not sure about weights, comput them via ComputeABfromC
-      std::cout << "Radau = " << Radau << ", weight = " << RadauWeight <<  std::endl;
-      
-      Vector<> Gauss2c(2), Gauss3c(3);
-    
-    
-
-    ExplicitEuler stepper(rhs);
-    ImplicitEuler stepper(rhs);
-
-    RungeKutta stepper(rhs, Gauss2a, Gauss2b, Gauss2c);
-
-    // Gauss3c .. points tabulated, compute a,b:
-    //auto [Gauss3a,Gauss3b] = ComputeABfromC (Gauss3c);
-    //ImplicitRungeKutta stepper(rhs, Gauss3a, Gauss3b, Gauss3c);
+  // Gauss3c .. points tabulated, compute a,b:
+  //ImplicitRungeKutta stepper(rhs, Gauss3a, Gauss3b, Gauss3c);
 
 
-    /*
-    // arbitrary order Gauss-Legendre
-    int stages = 5;
-    Vector<> c(stages), b1(stages);
-    GaussLegendre(c, b1);
+  /*
+  // arbitrary order Gauss-Legendre
+  int stages = 5;
+  Vector<> c(stages), b1(stages);
+  GaussLegendre(c, b1);
 
-    auto [a, b] = ComputeABfromC(c);
-    ImplicitRungeKutta stepper(rhs, a, b, c);
-    */
+  auto [a, b] = ComputeABfromC(c);
+  ImplicitRungeKutta stepper(rhs, a, b, c);
+  */
 
-    /* 
-    // arbitrary order Radau
-    int stages = 5;
-    Vector<> c(stages), b1(stages);
-    GaussRadau(c, b1);
+  /* 
+  // arbitrary order Radau
+  int stages = 5;
+  Vector<> c(stages), b1(stages);
+  GaussRadau(c, b1);
 
-    auto [a, b] = ComputeABfromC(c);
-    ImplicitRungeKutta stepper(rhs, a, b, c);
-    */
+  auto [a, b] = ComputeABfromC(c);
+  ImplicitRungeKutta stepper(rhs, a, b, c);
+  */
 
 
   for (int s : steps){
